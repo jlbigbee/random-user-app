a6734b6bc94f37c1c5434c259174e8673a360ccd
Jonathan Bigbee <bigbee.jonathan@gmail.com>
2024-06-25 02:13:38 -0700

    [BEEF-7] created RandomUsersTests

--- social-media-app/social-media-appTests/social_media_appTests.swift
+++ social-media-app/social-media-appTests/social_media_appTests.swift
@@ -34,3 +34,25 @@ final class social_media_appTests: XCTestCase {
     }
 
 }
+
+class RandomUsersTests: XCTestCase {
+  func testUserModel() throws {
+    let jsonData = getTestJSONData()
+
+    do {
+      _ = try JSONDecoder().decode(User.self, from: jsonData)
+    }
+    catch {
+      XCTFail("Failed to decode JSON into the model: \(error)")
+    }
+  }
+
+  private func getTestJSONData() -> Data {
+    guard let path = Bundle.main.path(forResource: "randomUsers",
+                                      ofType: "json") else {
+      fatalError("randomUsers.json file not found")
+    }
+    let internalURL = URL(fileURLWithPath: path)
+    return try! Data(contentsOf: internalURL)
+  }
+}
